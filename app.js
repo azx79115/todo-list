69a13f4814a3f7956a9b26ed2ce26ce516a8267c
azx79115 <azx79115@gmail.com>
2023-03-28 17:50:02 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -88,6 +88,15 @@ app.post('/todos/:id/edit', (req, res) => {
   .catch(error => console.log(error))
 })
 
+//delete路由設定
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+  .then(todo => todo.remove())
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
+})
+
 //設定port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
