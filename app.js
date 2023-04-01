78d729d6bdd465eccc0939c9a095e43a6bc29c84
azx79115 <azx79115@gmail.com>
2023-04-01 15:54:31 +0800

    add sort function

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find()//取出Todo model裡的所有資料
   .lean()// 把Mongoose的Model物件轉換成乾淨的javascript資料陣列
+  .sort({ _id: 'asc' }) //desc
   .then(todos => res.render('index', { todos }))//將資料傳給index樣板
   .catch(error => console.error(error))//錯誤處理
 })
@@ -75,8 +76,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-//將修改後的edit傳送至資料庫
-//收集checkbox路由
+//將修改後的edit傳送至資料庫,收集checkbox
 app.post('/todos/:id/edit', (req, res) => {
   console.log(req.body)
   const id = req.params.id
