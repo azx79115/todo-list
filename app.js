d3e969fb7affa7ba594f3c964a33c21261b34850
azx79115 <azx79115@gmail.com>
2023-04-02 18:44:06 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //載入express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
-//載入mongoose
-const mongoose = require('mongoose')
 //載入handlebars
 const exphbs = require('express-handlebars')
 //載入body-parser
@@ -12,35 +9,25 @@ const methodOverride = require('method-override')
 //引用路由器
 const routes = require('./routes')
 
+require('./config/mongoose')
+const app = express()
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 
-//用app.use 規定每一筆請求都需要透過body-parser進行前置處理
-app.use(bodyParser.urlencoded({ extened: true }))
+
 //hnadlebars引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+//用app.use 規定每一筆請求都需要透過body-parser進行前置處理
+app.use(bodyParser.urlencoded({ extened: true }))
 //設定每一筆請求都會透過methodOverride進行前置處理
 app.use(methodOverride('_method'))
 //將 request導入路由器
 app.use(routes)
-
-
-//設定連線到mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 
 //設定port 3000
