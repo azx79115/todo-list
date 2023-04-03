71f471eefb03088821f0434b93ef0d925332bf3e
azx79115 <azx79115@gmail.com>
2023-04-03 13:35:09 +0800

    feat:add bootstrap & modify main.hbs

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Todo = require('../../models/todo')
-
+//讀取新增介面
 router.get('/new', (req, res) => {
   return res.render('new')
 })
+//新增todo
 router.post('/', (req, res) => {
   const name = req.body.name
   return Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+//瀏覽特定資料
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -18,6 +20,7 @@ router.get('/:id', (req, res) => {
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
+//讀取編輯介面
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -25,6 +28,7 @@ router.get('/:id/edit', (req, res) => {
     .then(todo => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
+//修改todo資料
 router.put('/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
@@ -37,6 +41,7 @@ router.put('/:id', (req, res) => {
     .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
+//刪除todo資料
 router.delete('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
