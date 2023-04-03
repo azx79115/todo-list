62b70fd7a20fa3ae2efee917d1328552e327d245
azx79115 <azx79115@gmail.com>
2023-04-03 20:33:12 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -11,11 +11,8 @@ const routes = require('./routes')
 
 require('./config/mongoose')
 const app = express()
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+//如果在Heroku 環境則使用process.env.PORT；否則為本地環境使用3000
+const PORT = process.env.PORT || 3000
 
 
 
@@ -31,6 +28,6 @@ app.use(routes)
 
 
 //設定port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
